cda163782d177b87e8be399275b0c262ec347bf9
aml360 <aml360esp@gmail.com>
2021-03-29 13:58:47 +0200

    Menu lateral v1

--- CompNode/FrontComp/src/app/shared/menu/menu.component.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-	document.getElementById("sideNavigation").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-	document.getElementById("sideNavigation").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
-}
--- CompNode/FrontComp/src/app/shared/menu/menu.component.ts
+++ CompNode/FrontComp/src/app/shared/menu/menu.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 	constructor() {}
-
-	public validate() {
-		return true;
-	}
-
+	clase: string = 'collapse';
+	collapse(): void {}
 	ngOnInit(): void {}
 }
