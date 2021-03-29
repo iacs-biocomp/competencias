d2474d72f0ecb8a92dfeb4671d39406f80de535e
cizquierdo.iacs <cristinaizquierdo2000@gmail.com>
2021-03-29 10:05:21 +0200

    Cambios en el menu desplegable

---
+++ CompNode/FrontComp/src/app/shared/menu/menu.component.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById("sideNavigation").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+	document.getElementById("sideNavigation").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
