7a21962cb23a18a61b2e0659ad0b3d6503d5145f
cizquierdo.iacs <cristinaizquierdo2000@gmail.com>
2021-03-31 09:51:53 +0200

    Mis evaluaciones VISTA

--- CompNode/FrontComp/src/app/modules/evaluaciones/mis-evaluaciones/mis-evaluaciones.component.ts
+++ CompNode/FrontComp/src/app/modules/evaluaciones/mis-evaluaciones/mis-evaluaciones.component.ts
@@ -10,10 +10,17 @@ import { EvaluacionesService } from '../evaluaciones.service';
 export class MisEvaluacionesComponent implements OnInit {
 	evaluacionData!: IEvaluacion[];
 
+	//Pruebas para mostrar un texto u otro en los botones (evaluar o calcular)
+	buttonEvaluar: boolean = true;
+	buttonCalcular: boolean = true;
+
 	constructor(private evService: EvaluacionesService) {}
 
 	async ngOnInit(): Promise<void> {
 		this.evaluacionData = await this.evService.evaluacionesUsr('TEST');
+		this.buttonEvaluar = true;
+		this.buttonCalcular = true;
+
 
 		console.log(this.evaluacionData, await this.evService.evaluacionesUsr('TEST'));
 	}
