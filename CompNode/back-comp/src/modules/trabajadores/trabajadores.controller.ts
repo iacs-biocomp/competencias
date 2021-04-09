5d137145165a0d4d2154a300103cb94184fc83e8
aml360 <aml360esp@gmail.com>
2021-04-09 14:25:23 +0200

    Nest: Metodo para obtener la información de los trabajadores para la vista trabajadores-admin

--- CompNode/back-comp/src/modules/trabajadores/trabajadores.controller.ts
+++ CompNode/back-comp/src/modules/trabajadores/trabajadores.controller.ts
@@ -7,12 +7,31 @@ import { TrabajadorRepo } from './trabajador.repository';
 export class TrabajadoresController {
 	constructor(
 		@InjectRepository(TrabajadorRepo)
-		private readonly nivRepo: TrabajadorRepo,
+		private readonly trabRepo: TrabajadorRepo,
 	) {}
 
 	@Get('all')
-	getAllWorker(): Promise<Trabajador[]> {
-		return this.nivRepo.find({ relations: ['subModels'] });
+	async getAllWorker() {
+		var trabajadores = await this.trabRepo.find({ relations: ['periodos', 'periodos.catContr', 'periodos.catComp'] });
+		//TODO: Usar queryBuilder o buscar manera para seleccionar solo el periodo activo y no filtrar en codigo
+		//Filtro los periodos y escojo solo el activo
+		trabajadores.forEach(trab => (trab.periodos = trab.periodos.filter(p => p.actual)));
+		//Mapeo de Trabajador a ITrabajadorDTO
+		var trabDto: ITrabajadorDTO[] = [];
+		trabajadores.forEach(trab =>
+			trabDto.push({
+				dni: trab.dni,
+				apellidos: trab.apellidos,
+				area: trab.area,
+				catComp: trab.periodos[0].catComp.id,
+				catContr: trab.periodos[0].catContr.id,
+				departamento: trab.departamento,
+				nombre: trab.nombre,
+				unidad: trab.unidad,
+			}),
+		);
+		// trabajadores.map(t => t.periodos.filter(p => p.actual === true));
+		return trabDto;
 	}
 
 	//TODO: Completar para añadir los parametros como queryparams y no como el dni tal que :dni
@@ -24,12 +43,12 @@ export class TrabajadoresController {
 	): Promise<Trabajador> {
 		var relaciones = submodel ? ['subModel'] : [];
 		relaciones = evaluaciones ? ['subModel', 'subModel.evaluaciones'] : relaciones;
-		return this.nivRepo.findOne({ dni: dni }, { relations: relaciones });
+		return this.trabRepo.findOne({ dni: dni }, { relations: relaciones });
 	}
 
 	@Delete(':dni')
 	async deleteWorker(@Param('dni') dni: string): Promise<boolean> {
-		const worker = await this.nivRepo.findOne({ dni: dni }, { relations: ['subModels'] });
+		const worker = await this.trabRepo.findOne({ dni: dni }, { relations: ['subModels'] });
 
 		if (!worker) {
 			throw new NotFoundException('No existe ningun worker con ese dni');
@@ -43,16 +62,16 @@ export class TrabajadoresController {
 
 	@Post('')
 	async createTrabajador(@Body() worker: Trabajador): Promise<boolean> {
-		const existingTrabajador = this.nivRepo.findOne({ dni: worker.dni });
+		const existingTrabajador = this.trabRepo.findOne({ dni: worker.dni });
 		if (existingTrabajador) {
 			throw new ConflictException('Trabajador ya creada');
 		}
-		await this.nivRepo.save(worker);
+		await this.trabRepo.save(worker);
 		return true;
 	}
 	@Put('')
 	async updateWorker(@Body() worker: Trabajador): Promise<boolean> {
-		const existingTrabajador = this.nivRepo.findOne({ dni: worker.dni }, { relations: ['subModels'] });
+		const existingTrabajador = this.trabRepo.findOne({ dni: worker.dni }, { relations: ['subModels'] });
 		if (!existingTrabajador) {
 			throw new NotFoundException('No existe un worker con ese dni');
 		}
@@ -60,7 +79,25 @@ export class TrabajadoresController {
 		// if (worker.subModels.length !== 0) {
 		// 	throw new UnauthorizedException('Ese worker esta asociado a un submodelo, no se puede modificar');
 		// }
-		await this.nivRepo.save(worker);
+		await this.trabRepo.save(worker);
 		return true;
 	}
+}
+
+interface ITrabajadorDTO {
+	dni: string;
+
+	nombre: string;
+
+	apellidos: string;
+
+	area: string;
+
+	unidad: string;
+
+	departamento: string;
+
+	catComp: string;
+
+	catContr: string;
 }
