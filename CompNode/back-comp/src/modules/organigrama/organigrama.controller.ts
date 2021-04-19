1cc9cc0ffbd8ab13667cbb7dba41e50a3329f9f5
aml360 <aml360esp@gmail.com>
2021-04-19 14:52:17 +0200

    Nest: Refactor en organigrama. Ng: Cambios en mis datos

--- CompNode/back-comp/src/modules/organigrama/organigrama.controller.ts
+++ CompNode/back-comp/src/modules/organigrama/organigrama.controller.ts
@@ -25,51 +25,41 @@ export class OrganigramaController {
 
 	@Get('all')
 	async getAll(): Promise<UsrWithOrgani[]> {
-		var workers = await this.trabRepo.find({
+		const workers = await this.trabRepo.find({
 			relations: ['periodos', 'periodos.superiores', 'periodos.inferiores', 'periodos.pares'],
 		});
-		var fullOrgani: UsrWithOrgani[] = [];
-
-		//Filtro los periodos de cada trabajador y me quedo solo con el actual (Como array)
 		const organiFull: UsrWithOrgani[] = workers.map(wrk => {
+			//Filtro el periodo al actual
 			wrk.periodos = wrk.periodos.filter(per => per.actual);
-			return {
+			const org: UsrWithOrgani = {
 				inferiores: wrk.periodos[0].inferiores,
 				superiores: wrk.periodos[0].superiores,
 				pares: wrk.periodos[0].pares,
 				trabajador: wrk,
 			};
+			//Borro los periodos de cada trabajador (Info no necesaria)
+			delete org.trabajador.periodos;
+			return org;
 		});
-		organiFull.forEach(org => delete org.trabajador.periodos);
-		// workers.forEach(wrk => {
-		// 	wrk.periodos = wrk.periodos.filter(per => per.actual);
-		// 	fullOrgani.push({
-		// 		inferiores: wrk.periodos[0].inferiores,
-		// 		superiores: wrk.periodos[0].superiores,
-		// 		pares: wrk.periodos[0].pares,
-		// 		//Añado undefined para luego quitarle el campo periodos
-		// 		trabajador: undefined,
-		// 	});
-		// });
-		// for (const i in fullOrgani) {
-		// 	delete workers[i].periodos;
-		// 	fullOrgani[i].trabajador = workers[i];
-		// }
 		return organiFull;
 	}
 
 	//TODO: Completar para añadir los parametros como queryparams y no como el dni tal que :dni
 	@Get(':username')
 	async getOrganigramaUsr(@Param('username') username: string): Promise<Organigrama> {
-		const usr = await this.usrRepo.findOne({ username: username }, { relations: ['trabajador'] });
-		const dni = usr.trabajador.dni;
-		const worker = await this.trabRepo.findOne(
-			{ dni: dni },
+		const usr = await this.usrRepo.findOne(
+			{ username: username },
 			{
-				relations: ['periodos', 'periodos.superiores', 'periodos.inferiores', 'periodos.pares'],
+				relations: [
+					'trabajador',
+					'trabajador.periodos',
+					'trabajador.periodos.superiores',
+					'trabajador.periodos.inferiores',
+					'trabajador.periodos.pares',
+				],
 			},
 		);
-
+		const worker = usr.trabajador;
 		const actualPer: PeriodoTrab = worker.periodos.filter(per => per.actual)[0];
 		const organi: Organigrama = {
 			inferiores: actualPer.inferiores,
