5e2fa68fe2d794e5b90105c4478f4fd4e043e00e
cizquierdo.iacs <cristinaizquierdo2000@gmail.com>
2021-05-06 14:57:08 +0200

    Guardado de submodelos (ya se muestran correctamente los comportamientos añadidos por competencia y nivel)

--- CompNode/FrontComp/src/app/modules/admin/evaluaciones-admn/modelos/new-ev-model.component.ts
+++ CompNode/FrontComp/src/app/modules/admin/evaluaciones-admn/modelos/new-ev-model.component.ts
@@ -6,6 +6,7 @@ import { CompetenciasService } from '../../competencias-admin/services/competenc
 import { CatCompetencialesService } from '../../cat-admn/services/CatCompetenciales.service';
 import { ComportService } from '../../comportamientos-admin/services/comport.service';
 import { NivelService } from '../../niveles-admin/services/nivel.service';
+import { EvaluacionesAdmService } from '../services/evaluaciones-adm.service';
 
 type DbData = {
 	/** listado de categorias competenciales */
@@ -64,12 +65,12 @@ export class NewEvModelComponent implements OnInit {
 		nivObjetivoValidator: (competencias: MiCompetencia[]) => {
 			var valid = true;
 			competencias.forEach(c => {
-				console.log(c.nivObjetivo);
+				// console.log(c.nivObjetivo);
 				if (!c.nivObjetivo) {
 					valid = false;
 				}
 			});
-			console.log(valid);
+			// console.log(valid);
 			return valid;
 		},
 	};
@@ -78,6 +79,8 @@ export class NewEvModelComponent implements OnInit {
 	competenciasSelect: MiCompetencia[] = [];
 	/** Guarda la lista de comportamientos seleccionados */
 	comportamientosSelect: MiComportamiento[] = [];
+	/** Guarda la categoria competencial seleccionada */
+	catCompeteSelected!: ICatComp;
 
 	/**
 	 * Modelo a guardar en la bbdd, es la 'referencia'
@@ -143,6 +146,7 @@ export class NewEvModelComponent implements OnInit {
 		private competSv: CompetenciasService,
 		private nivSv: NivelService,
 		private comportSv: ComportService,
+		private evModelSv: EvaluacionesAdmService,
 	) {}
 
 	async ngOnInit(): Promise<void> {
@@ -157,15 +161,17 @@ export class NewEvModelComponent implements OnInit {
 		this.dbData.niveles = promises[2];
 		this.dbData.comports = promises[3];
 		// setInterval(() => console.log(this.comportCtl), 5500);
-		setInterval(() => console.log(this.competenciasSelect), 5500);
+		// setInterval(() => console.log(this.competenciasSelect), 5500);
+		// setInterval(() => console.log(this.dbData.modelToAdd), 5500);
 	}
 
 	/** Selecciona la cat competen del modelo */
 	selectCatComp(catCompet: ICatComp) {
-		if (catCompet) {
-			this.dbData.modelToAdd.catComp = catCompet;
-			console.log(this.dbData.modelToAdd.catComp);
+		if (!catCompet) {
+			return;
 		}
+		this.catCompeteSelected = catCompet;
+		this.dbData.modelToAdd.catComp = this.catCompeteSelected;
 	}
 
 	/** Selecciona las competencias del submodelo */
@@ -173,6 +179,7 @@ export class NewEvModelComponent implements OnInit {
 		const index = this.competenciasSelect.indexOf(compete);
 		if (index == -1) {
 			this.competenciasSelect.push(compete);
+			// console.log(this.dbData.comps[index]);
 		} else {
 			this.competenciasSelect.splice(index, 1);
 		}
@@ -183,6 +190,7 @@ export class NewEvModelComponent implements OnInit {
 		const arrToPush = this.comportCtl.comportsSelected;
 		const index = this.comportCtl.comportsSelected!.indexOf(comport);
 		if (index == -1) {
+		//	console.log(`adding ${comport}`);
 			arrToPush.push(comport);
 		} else {
 			arrToPush.splice(index, 1);
@@ -210,7 +218,10 @@ export class NewEvModelComponent implements OnInit {
 		}
 	}
 
+
 	addAllComports(): void {
+	// console.log(`ForEach comportsSelected `);
+  // console.log(this.comportCtl.comportsSelected);
 		this.comportCtl.comportsSelected.forEach(comport =>
 			this.addComportToCompet(this.comportCtl.compSelected!, this.comportCtl.nivSelected!, comport),
 		);
@@ -220,6 +231,7 @@ export class NewEvModelComponent implements OnInit {
 	 * Añade un comportamiento con un nivel asociado a una competencia
 	 */
 	addComportToCompet(comp: ICompetencia, niv: INivel, comport: IComportamiento): void {
+	//	console.log(comp, niv, comport);
 		let matchSubModel = this.dbData.modelToAdd.subModels.find(
 			x => x.competencia?.id === comp.id && x.nivel?.id === niv.id,
 		);
@@ -229,6 +241,7 @@ export class NewEvModelComponent implements OnInit {
 				competencia: comp,
 				comportamientos: [],
 			};
+			this.dbData.modelToAdd.subModels.push(matchSubModel);
 		}
 		const comportIndx = matchSubModel.comportamientos?.indexOf(comport);
 		if (comportIndx === -1) {
@@ -236,4 +249,48 @@ export class NewEvModelComponent implements OnInit {
 		}
 		return;
 	}
+
+	findSubModel(subModels: ISubModel[], comp: ICompetencia, niv: INivel) {
+		const oe = subModels.find(subModel => subModel.competencia === comp && subModel.nivel === niv);
+	// console.log(oe);
+	// console.log(subModels);
+		return oe;
+	}
+	findSubModels(subModels: ISubModel[], comp: ICompetencia) {
+		const oe = subModels.filter(subModel => subModel.competencia === comp);
+  //console.log(oe);
+	//console.log(subModels);
+		return oe;
+	}
+	getAllComportsOfComp(comp: ICompetencia, subModels: ISubModel[]): IComportamiento[] {
+		const subModelos = this.findSubModels(subModels, comp);
+		let comports:IComportamiento[] = [];
+		subModelos.forEach((s)=>comports.concat(s.comportamientos!));
+		return comports;
+	}
+	printComports(comp: ICompetencia, subModels: ISubModel[]): IComportamiento[]{
+		let comports:IComportamiento[] = [];
+		let matchComport = this.getAllComportsOfComp(comp, subModels).find(x=> x.subModels == subModels);
+		console.log(matchComport);
+		if (!matchComport){
+			return comports.filter(x=>!matchComport);
+		}
+		else {
+			return comports.filter(x=>!matchComport);
+		}
+	}
+
+	/* 	saveData() {
+		this.dbData.modelToAdd = {
+			catComp: this.catCompeteSelected,
+			subModels: [
+				{
+					nivel: this.comportCtl.nivSelected,
+					competencia: this.comportCtl.compSelected,
+					comportamientos: this.comportCtl.comportsSelected,
+				},
+			],
+		};
+		this.modelSV.save
+	} */
 }
