3c2707203db500e3fc10fb8cd572f1cc847144e6
cizquierdo.iacs <cristinaizquierdo2000@gmail.com>
2021-05-20 14:50:08 +0200

    Comentarios añadidos en utility

--- CompNode/sharedCode/Utility.ts
+++ CompNode/sharedCode/Utility.ts
@@ -1,11 +1,22 @@
 import { ICompetencia, INivel } from './interfaces/Entity';
 
-// TODO: Tsdoc
+/**
+ * Busca un nivel en un array de niveles donde le indicamos el id del nivel
+ *
+ * @param niveles array de niveles
+ * @param nivId id del nivel que queremos buscar
+ * @returns si ha encontrado un nivel con ese id, retorna el nivel, si no undefined
+ */
 export function findNivelById(niveles: INivel[], nivId: string): INivel | undefined {
 	return niveles.find(nivel => nivel.id === nivId);
 }
-// TODO: Tsdoc
-/** */
+/**
+ * Busca una competencia en un array de competencias donde le indicamos el id de la competencia
+ *
+ * @param competencias array de competencias
+ * @param compId id de la competencia que queremos buscar
+ * @returns si ha encontrado una competencia con ese id la devuelve, si no undefined
+ */
 export function findCompById(competencias: ICompetencia[], compId: string): ICompetencia | undefined {
 	return competencias.find(comp => comp.id === compId);
 }
