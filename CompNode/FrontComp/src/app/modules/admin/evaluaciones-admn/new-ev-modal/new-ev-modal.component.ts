4fc6e9f946f480c09cf1ee9ce6cfb05e9a958ff3
aml360 <aml360esp@gmail.com>
2021-06-02 09:09:57 +0200

    style(ng-nest): formateo de todo el codigo con prettier

--- CompNode/FrontComp/src/app/modules/admin/evaluaciones-admn/new-ev-modal/new-ev-modal.component.ts
+++ CompNode/FrontComp/src/app/modules/admin/evaluaciones-admn/new-ev-modal/new-ev-modal.component.ts
@@ -41,13 +41,13 @@ export class NewEvModalComponent implements OnInit {
 		private fb: FormBuilder,
 	) {}
 
-		/**
+	/**
 		 * async ngOnInit(): Promise<void> {
 		const promises = await Promise.all([this.cCompSv.getAll(), this.evModelSv.getAll()]);
 		this.catComps = promises[0].sort((a, b) => a.id.localeCompare(b.id));
 		this.evModels = promises[1];
 		 */
- 	async ngOnInit(): Promise<void> {
+	async ngOnInit(): Promise<void> {
 		this.allReferenceModels = await this.evModelSv.getAllReference();
 		this.catComps = this.allReferenceModels.map(refModel => refModel.catComp);
 		console.log(this.catComps);
