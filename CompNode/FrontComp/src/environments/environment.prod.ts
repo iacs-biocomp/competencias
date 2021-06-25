b0e10f1fbf751602917f0782aa2856b66b56d526
Carlos <ctelleria@aragon.es>
2021-06-25 14:24:39 +0200

    Cambio en generación de imágenes docker

--- CompNode/FrontComp/src/environments/environment.prod.ts
+++ CompNode/FrontComp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	/**URL donde la api (nest) estará localizada en producción. */
-	apiURL: 'http://localhost/nest',
+	apiURL: 'http://yourdomainname.com/nest',
 	/** El intervalo de tiempo en `ms` en el cual se renovará el jwt si ha habido interacción del usuario */
 	jwtInterval: 45000,
 };
