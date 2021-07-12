2d09313e882f77168e60ac5ae8eb7bf648726bf4
aml360 <aml360esp@gmail.com>
2021-07-12 11:38:04 +0200

    build(ng-nest): bump dependencies, nestjs v8 https://is.gd/A94p5z, rxjs v7 backend, latest angular
    Cambiado databaseModule para funcionar con nestjs v8, funciona pero no me queda del todo claro
    porque da errores, mirar en un futuro los ejemplos de dbProviders
    https://github.com/nestjs/nest/tree/master/sample.

--- back-comp/src/database/database.module.ts
+++ back-comp/src/database/database.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.service';
-
+import { databaseProviders } from './database.providers';
 @Module({
 	imports: [...databaseProviders],
-	exports: [...databaseProviders],
+	// ! Error al exportarlo en nestjs v8 pero funciona sin exportar https://is.gd/A94p5z
+	// exports: [...databaseProviders],
 })
 export class DatabaseModule {}
