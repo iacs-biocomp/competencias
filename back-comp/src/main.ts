74db397e2a7e4b51803aa1f9dc0029847a8021ff
aml360 <aml360esp@gmail.com>
2021-07-13 11:49:23 +0200

    feat(nest): llamada api unix time para comprobar fecha servidor

--- back-comp/src/main.ts
+++ back-comp/src/main.ts
@@ -5,10 +5,30 @@ import { Configuration as cnf } from './config/config.keys';
 import { ConfigService } from './config/config.service';
 import { install } from 'source-map-support';
 import { Promise } from 'bluebird';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { differenceInSeconds, fromUnixTime } from 'date-fns';
 
 const cnfService = new ConfigService();
+const http = new HttpService();
 
+async function isSvDateCorrect(httpSv: HttpService): Promise<boolean> {
+	const httpJsonResponse = await firstValueFrom(httpSv.get<{ unixtime: string }>('http://worldtimeapi.org/api/ip'));
+	const dateActual = new Date();
+	const dateHttp = fromUnixTime(Number.parseInt(httpJsonResponse.data.unixtime));
+	const dateDiff = differenceInSeconds(dateActual, dateHttp);
+	if (dateDiff > 30 || dateDiff < -30) {
+		console.error(
+			`\x1b[31mLa fecha del servidor difiere con la de la api en ${dateDiff} segundos [negativo es atrasado el sv]`,
+		);
+		return false;
+	} else {
+		return true;
+	}
+}
 async function bootstrap() {
+	//Comprobación fecha servidor
+	await isSvDateCorrect(http);
 	install();
 	if (process.env.NODE_ENV !== 'production') {
 		//Las promesas de node no muestran un stacktrace descriptivo, se usan estas,
