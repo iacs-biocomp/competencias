2105afeb9e3a468d862db0949c5535f0aa12ca23
Adrian Molina <aml360esp@gmail.com>
2021-07-29 09:57:30 +0200

    fix(back): fix posible exceptions when checking the server time

--- back-comp/src/main.ts
+++ back-comp/src/main.ts
@@ -13,19 +13,25 @@ const cnfService = new ConfigService();
 const http = new HttpService();
 
 async function isSvDateCorrect(httpSv: HttpService): Promise<boolean> {
-	const httpJsonResponse = await firstValueFrom(httpSv.get<{ unixtime: string }>('http://worldtimeapi.org/api/ip'));
-	const dateActual = new Date();
-	const dateHttp = fromUnixTime(Number.parseInt(httpJsonResponse.data.unixtime));
-	const dateDiff = differenceInSeconds(dateActual, dateHttp);
-	if (dateDiff > 30 || dateDiff < -30) {
-		console.error(
-			`\x1b[31mLa fecha del servidor difiere con la de la api en ${dateDiff} segundos [negativo es atrasado el sv]`,
-		);
+	try {
+		const httpJsonResponse = await firstValueFrom(httpSv.get<{ unixtime: string }>('http://worldtimeapi.org/api/ip'));
+		const dateActual = new Date();
+		const dateHttp = fromUnixTime(Number.parseInt(httpJsonResponse.data.unixtime));
+		const dateDiff = differenceInSeconds(dateActual, dateHttp);
+		if (dateDiff > 30 || dateDiff < -30) {
+			console.error(
+				`\x1b[31mLa fecha del servidor difiere con la de la api en ${dateDiff} segundos [negativo es atrasado el sv]`,
+			);
+			return false;
+		} else {
+			return true;
+		}
+	} catch (error) {
+		console.error(error);
 		return false;
-	} else {
-		return true;
 	}
 }
+
 async function bootstrap() {
 	//Comprobación fecha servidor
 	await isSvDateCorrect(http);
