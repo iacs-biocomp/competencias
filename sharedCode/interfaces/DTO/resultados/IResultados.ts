a3b2a27f1423e696d72d6362b6ddad540a98001d
aml360 <aml360esp@gmail.com>
2021-08-09 12:58:16 +0200

    refactor(shared_interfaces): refactor IResultadoDTO to v2

--- sharedCode/interfaces/DTO/resultados/IResultados.ts
+++ sharedCode/interfaces/DTO/resultados/IResultados.ts
@@ -7,3 +7,15 @@ export interface IResultadoDTO {
 	competencia: string;
 	// clientes?:
 }
+
+export interface IResultadoDTOV2 {
+	maxResult: number;
+	minResult: number;
+	values: Array<{
+		// TODO: tsdoc, es quien compone el result, por ejemplo inferiores, pares, superiores, externos, clientes, etc
+		name: string;
+		// TODO: tsdoc, valor del resultado, por ejemplo 20, 35.43, -10.5, siempre en rango de maxResult y minResult
+		value: number;
+	}>;
+	competencia: string;
+}
