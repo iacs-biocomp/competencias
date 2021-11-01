b96081e0351d375b4b83a711f4a2e87553390b12
aml360 <aml360esp@gmail.com>
2021-11-01 22:26:08 +0100

    refactor(back): remove unused code for testing from app.controller

--- back-comp/src/app.controller.ts
+++ back-comp/src/app.controller.ts
@@ -1,18 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { LoggingService } from './modules/logging/services/logging.service';
 
 @Controller('api')
 export class AppController {
 	constructor(private readonly logger: LoggingService) {}
-	// private readonly logger = new Logger(AppController.name);
-
-	@Get()
-	test(): string {
-		this.logger.verbose('verbose msg');
-		this.logger.debug('debug msg');
-		this.logger.error('error stuff');
-		this.logger.warn('warning stuff');
-		this.logger.log('logging level');
-		return 'Hello!';
-	}
+	// @Get()
+	// test(): void {}
 }
