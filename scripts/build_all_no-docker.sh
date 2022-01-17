0a5dc66b6b80dab787774e434bb2359eb888e288
aml360 <aml360esp@gmail.com>
2022-01-17 15:10:57 +0100

    ci: change to github actions for ci/cd

--- scripts/build_all_no-docker.sh
+++ scripts/build_all_no-docker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 GO_TO=$(dirname "$0")
 cd $GO_TO
+
 function build_front {
     cd ../FrontComp/;
-    pnpm i;
-    pnpm run build:prod
+    npm i;
+    npm run build:prod
 }
 function build_back {
     cd ../back-comp/
-    pnpm i;
-    pnpm run build:prod
+    npm i;
+    npm run build:prod
 }
 
 build_front &
