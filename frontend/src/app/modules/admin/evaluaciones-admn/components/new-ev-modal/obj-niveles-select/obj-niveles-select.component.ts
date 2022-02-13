f7e25fbe7e1d73650ad4d2034cf080528fbec346
aml360 <aml360esp@gmail.com>
2022-02-13 20:02:24 +0100

    refactor: refactor ObjectiveNivsSelectComponent

--- frontend/src/app/modules/admin/evaluaciones-admn/components/new-ev-modal/obj-niveles-select/obj-niveles-select.component.ts
+++ frontend/src/app/modules/admin/evaluaciones-admn/components/new-ev-modal/obj-niveles-select/obj-niveles-select.component.ts
@@ -9,14 +9,16 @@ import {
 	EventEmitter,
 } from '@angular/core';
 import { BehaviorSubject, Subscription } from 'rxjs';
-import { NivelService } from 'services/data';
 import { ICompetencia, INivel } from 'sharedInterfaces/Entity';
 import { LogService } from 'src/app/shared/log/log.service';
 
 /** Tipo formado por una competencia y su nivel asociado */
 export type CompAndNiv = { comp: ICompetencia; niv: INivel };
+
+type CompAndOptNiv = Pick<CompAndNiv, 'comp'> & Partial<CompAndNiv>;
+
 /** Config type for the component itself */
-type cConfig = {
+export type cConfig = {
 	/** Title modelNiv */
 	title: string;
 };
@@ -25,7 +27,7 @@ type cConfig = {
  * Componente que muestra una lista de competencias y al lado un desplegable para selecionar un nivel
  */
 @Component({
-	selector: 'app-obj-nivs-select [idModal] [compsObs]',
+	selector: 'app-obj-nivs-select [idModal] [compsObs] [referenceLevels]',
 	templateUrl: './obj-niveles-select.component.html',
 	styleUrls: ['./obj-niveles-select.component.scss'],
 })
@@ -33,7 +35,7 @@ export class ObjectiveNivsSelectComponent implements OnInit, OnDestroy {
 	/** Receives an array with the competencies selected to the evaluation */
 	@Input() compsObs!: BehaviorSubject<ICompetencia[]>;
 	/** Receives an array with the levels to show next to each competence */
-	@Input() nivsObs!: BehaviorSubject<INivel[]>;
+	@Input() referenceLevels: INivel[] = [];
 	/** Emits array of {@link CompAndNiv} (objective level paired to a comp) */
 	@Output('onSaved') compAndNivEmitter = new EventEmitter<CompAndNiv[]>();
 	/** The identifier used for toggle the modal */
@@ -47,35 +49,23 @@ export class ObjectiveNivsSelectComponent implements OnInit, OnDestroy {
 	#subs: Subscription[] = [];
 
 	/**
-	 * Array que almacena todos lo niveles actuales
-	 * @deprecated Usar el observable de niveles como input del componente
-	 * TODO: [9]{N1} modificar para usar el observable de niveles
-	 *
-	 */
-	nivs: INivel[] = [];
-
-	/** CompAndNiv[](comp, niv) partial  */
-
-	/**
 	 * partial(propiedades en opcional) de objeto compAndNiv (comp, niv)
 	 */
-	bufferCompNiv: Partial<CompAndNiv>[] = [];
+	bufferCompNiv: CompAndOptNiv[] = [];
 
-	constructor(private nivelSv: NivelService, private readonly logger: LogService) {}
+	constructor(private readonly logger: LogService) {}
 
-	async ngOnInit(): Promise<void> {
-		this.logger.verbose('Cargando componente obj-niveles-select');
-		this.nivs = await this.nivelSv.getAllRefNivs(); // Se guardan en la var niveles los niveles de referencia
+	ngOnInit(): void {
+		this.logger.verbose(`Executing ngOnInit ${this.constructor.name}`);
 		this.#subs.push(
 			this.compsObs.subscribe(comps => {
-				const compToSave = comps.map(c => ({ comp: c })) as Partial<CompAndNiv>[]; //Map the coompetences and saves it in compToSave
-				this.bufferCompNiv = compToSave;
+				this.bufferCompNiv = comps.map<CompAndOptNiv>(c => ({ comp: c })); //Map the coompetences and saves it in compToSave
 			}),
 		);
 	}
 
 	ngOnDestroy(): void {
-		this.logger.verbose('Destruyendo componente');
+		this.logger.verbose(`Executing ngOnDestroy of ${this.constructor.name} `);
 		this.#subs.forEach(s => s.unsubscribe());
 		this.closeModalBtn.nativeElement.click();
 	}
@@ -83,26 +73,26 @@ export class ObjectiveNivsSelectComponent implements OnInit, OnDestroy {
 	/**
 	 * Saves the objective level with their competence
 	 *
-	 * @param comp Competence with which the objective level is associated
-	 * @param niv The objective level or its code (NOT id) that will be associated with the competence
+	 * @param compId Competence with which the objective level is associated
+	 * @param nivId The objective level or its code (NOT id) that will be associated with the competence
 	 * @throws Error if objective level is not valid
 	 */
-	setNivel(comp: ICompetencia, niv: INivel | string): void {
-		this.logger.debug(`Guardando competencias con su nivel objetivo`, comp, niv);
-		const nivId = typeof niv === 'string' ? niv : niv.code;
-		const nivObj = this.nivs.find(n => n.code === nivId);
+	setNivel(compId: ICompetencia['id'], nivId: INivel['code']): void {
+		const nivObj = this.referenceLevels.find(n => n.code === nivId);
+		const indx = this.bufferCompNiv.findIndex(cn => cn.comp.id === compId);
 		if (!nivObj) {
 			throw new Error('Nivel objetivo no valido, codigo u objeto incorrecto');
 		}
-		const indx = this.bufferCompNiv.findIndex(cn => cn.comp?.id === comp.id);
+		if (indx === -1) {
+			throw new Error(`Competencia con id ${compId} no encontrada en bufferCompNiv`);
+		}
+		this.logger.debug(`Guardando competencias con su nivel objetivo`, compId, nivId);
 		this.bufferCompNiv[indx].niv = nivObj;
 	}
 
 	/** Creates the object type CompAndNiv[comp,niv] and emits it */
 	nivelClicked(): void {
-		const objCompNivel = this.bufferCompNiv.filter(objToCreate =>
-			!objToCreate.comp && !objToCreate.niv ? false : true,
-		) as CompAndNiv[]; // creates the object if is true
+		const objCompNivel = this.bufferCompNiv.filter<CompAndNiv>((v): v is CompAndNiv => !!v.niv && !!v.comp); // creates the object if is true
 		this.logger.verbose('Emitiendo nivel objetivo por competencia');
 		this.compAndNivEmitter.emit(objCompNivel); //emit the object to the parent
 	}
